1f34cc696fb004b4fb57df4bec91ff51090b6c4d
Weronika Kansy <weronika.kansy@gmail.com>
2023-11-16 20:47:16 +0100

    Apply classNames to letter slots using statuses returned by the checkGuess function

--- src/components/Guess/Guess.js
+++ src/components/Guess/Guess.js
@@ -1,10 +1,16 @@
 import { range } from '../../utils';
+import { checkGuess } from '../../game-helpers';
 
-function Guess({ guess }) {
+function Guess({ guess, answer }) {
+  const result = checkGuess(guess, answer);
+
   return (
     <p className="guess">
       {range(5).map((num) => (
-        <span key={num} className="cell">
+        <span
+          key={num}
+          className={guess ? `cell ${result[num].status}` : 'cell'}
+        >
           {guess ? guess[num] : null}
         </span>
       ))}
